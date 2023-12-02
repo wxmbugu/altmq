9672742bb2001120fb53c836104fc84a11dbce2a
wxmbugu <wambugumacharia35@gmail.com>
2023-12-03 01:31:03 +0300

    main
    Signed-off-by: wxmbugu <wambugumacharia35@gmail.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -23,38 +23,19 @@ fn handle_client(mut stream: TcpStream) {
 
         let received_data = String::from_utf8_lossy(&buffer[..bytes_read]);
         println!("Received data: {}", received_data);
-
+        let output = decode(&buffer);
+        println!("{output:?}");
         if received_data.trim() == "quit" {
             println!("Received exit command. Closing the connection.");
             break;
         }
-        let command = sanitize_commands(&received_data);
-        println!("Sanitized command: {}", command);
-
-        let cmd = sanitize_numeric_command(&received_data);
-        println!("Sanitized numeric command: {}", cmd);
-
         if let Err(err) = stream.write_all(&buffer[..bytes_read]) {
             eprintln!("Error writing to stream: {:?}", err);
             break;
         }
     }
 }
-fn sanitize_commands(command: &str) -> Commands {
-    match command.trim().to_lowercase().as_str() {
-        "subscribe" => Commands::SUBSCRIBE,
-        "send" => Commands::SEND,
-        // "topic" => Commands::TOPIC,
-        // "message" => Commands::MESSAGE,
-        _ => Commands::UNKNOWN(command.to_string()),
-    }
-}
-fn sanitize_numeric_command(received_data: &str) -> Commands {
-    match received_data.trim().parse::<u8>() {
-        Ok(num) => Commands::from_u8(num),
-        Err(_) => Commands::UNKNOWN(format!("Invalid numeric command: {}", received_data)),
-    }
-}
+
 static PORT: &str = "127.0.0.1:8000";
 
 fn main() -> std::io::Result<()> {
